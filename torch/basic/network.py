6f02e966a44f2928ed19a12a0824744b52da9aaf
Song S.H <37033795+clastro@users.noreply.github.com>
2022-07-24 19:10:35 +0900

    Update network.py

--- torch/basic/network.py
+++ torch/basic/network.py
@@ -13,3 +13,19 @@ class Net(nn.Module):
     x = self.fc2(x)
     x = self.output(x)
     return x
+
+import torch.nn.functional as F
+
+# Define another class Net
+class Net(nn.Module):
+    def __init__(self):    
+    	# Define all the parameters of the net
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(28 * 28 * 1, 200)
+        self.fc2 = nn.Linear(200,10)
+
+    def forward(self, x):   
+    	# Do the forward pass
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
