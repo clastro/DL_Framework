19c6f4eb6da3f76b01325379b4ed9811151cdfbe
Song S.H <37033795+clastro@users.noreply.github.com>
2022-07-24 22:38:20 +0900

    Create train_eval.py

---
+++ torch/basic/train_eval.py
@@ -0,0 +1,33 @@
+# Instantiate the Adam optimizer and Cross-Entropy loss function
+model = Net()   
+optimizer = optim.Adam(model.parameters(), lr=3e-4)
+criterion = nn.CrossEntropyLoss()
+  
+for batch_idx, data_target in enumerate(train_loader):
+    data = data_target[0]
+    target = data_target[1]
+    data = data.view(-1, 28 * 28)
+    optimizer.zero_grad()
+
+    # Complete a forward pass
+    output = model(data)
+
+    # Compute the loss, gradients and change the weights
+    loss = criterion(output, target)
+    loss.backward()
+    optimizer.step()
+    
+# Set the model in eval mode
+model.eval()
+
+for i, data in enumerate(test_loader, 0):
+    inputs, labels = data
+    # Put each image into a vector
+    inputs = inputs.view(-1, 28 * 28)
+    
+    # Do the forward pass and get the predictions
+    outputs = model(inputs)
+    _, outputs = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (outputs == labels).sum().item()
+print('The testing set accuracy of the network is: %d %%' % (100 * correct / total))
