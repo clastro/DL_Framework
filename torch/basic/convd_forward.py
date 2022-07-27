b66121f3025d5074fa69046404dfb78b72dc4f44
Song S.H <37033795+clastro@users.noreply.github.com>
2022-07-27 23:13:16 +0900

    Create convd_forward.py

---
+++ torch/basic/convd_forward.py
@@ -0,0 +1,29 @@
+class Net(nn.Module):
+    def __init__(self, num_classes):
+        super(Net, self).__init__()
+		
+        # Instantiate the ReLU nonlinearity
+        self.relu = nn.ReLU()
+        
+        # Instantiate two convolutional layers
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=5, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=5, out_channels=10, kernel_size=3, padding=1)
+        
+        # Instantiate a max pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        # Instantiate a fully connected layer
+        self.fc = nn.Linear(7 * 7 * 10, 10)
+
+    def forward(self, x):
+
+        # Apply conv followd by relu, then in next line pool
+        x = self.relu(self.conv1(x))
+        x = self.pool(x)
+
+        # Apply conv followd by relu, then in next line pool
+        x = self.relu(self.conv2(x))
+        x = self.pool(x)
+
+        # Prepare the image for the fully connected layer
+        x = x.view(-1, 7 * 7 * 10)
