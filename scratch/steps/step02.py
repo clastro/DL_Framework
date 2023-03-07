2f6a3abd11690b24a8bc4cad385d107dce8cecc9
Song S.H <37033795+clastro@users.noreply.github.com>
2023-03-08 08:14:18 +0900

    Create step02.py

---
+++ scratch/steps/step02.py
@@ -0,0 +1,8 @@
+class Function:
+  def __call__(self,input):
+    x = input.data
+    y = self.forward(x)
+    output = Variable(y)
+    return output
+  def forward(self, x):
+    raise NotImplementedError()
