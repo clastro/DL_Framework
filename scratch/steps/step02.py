9dcf0b8e2501836b31bd140ef9927e80608da8cf
Song S.H <37033795+clastro@users.noreply.github.com>
2023-03-08 10:12:48 +0900

    Update step02.py

--- scratch/steps/step02.py
+++ scratch/steps/step02.py
@@ -1,12 +1,12 @@
 class Function:
-  def __call__(self,input):
-    x = input.data
-    y = self.forward(x)
-    output = Variable(y)
-    return output
-  def forward(self, x):
-    raise NotImplementedError()
+    def __call__(self,input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+    def forward(self, x):
+        raise NotImplementedError()
 
-class Square(Function):
-  def forward(self,x):
-    return x ** 2
+class Square(Function): #Function을 상속하고 forward 함수 정의
+    def forward(self,x):
+        return x ** 2
