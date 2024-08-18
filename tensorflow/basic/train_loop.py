577d452b031472dfdccf3c67cba42e91d3b51046
Song S.H. <37033795+clastro@users.noreply.github.com>
2024-08-18 21:14:53 +0900

    Create train_loop.py

---
+++ tensorflow/basic/train_loop.py
@@ -0,0 +1,21 @@
+def train_step(inputs):
+  images, labels = inputs
+
+  with tf.GradientTape() as tape:
+    predictions = model(images, training=True)
+    loss = compute_loss(labels, predictions)
+
+  gradients = tape.gradient(loss, model.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+  train_accuracy.update_state(labels, predictions)
+  return loss 
+
+def test_step(inputs):
+  images, labels = inputs
+
+  predictions = model(images, training=False)
+  t_loss = loss_object(labels, predictions)
+
+  test_loss.update_state(t_loss)
+  test_accuracy.update_state(labels, predictions)
