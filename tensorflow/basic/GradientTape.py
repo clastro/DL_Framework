c1a20605f0db84b4f6a4b5cc26b60f851f97cc19
Song S.H. <37033795+clastro@users.noreply.github.com>
2024-10-30 23:27:52 +0900

    Update GradientTape.py

--- tensorflow/basic/GradientTape.py
+++ tensorflow/basic/GradientTape.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 
 def compute_gradient(x0):
-  	# Define x as a variable with an initial value of x0
+	# Define x as a variable with an initial value of x0
 	x = tf.Variable(x0)
 	with GradientTape() as tape: # 연산과정을 tape에 기록
 		tape.watch(x)
-        # Define y using the multiply operation
+	# Define y using the multiply operation
 		y = multiply(x,x)
     # y = x*x
     # Return the gradient of y with respect to x
