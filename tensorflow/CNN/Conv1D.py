98660ba399b81316332da621ee9cf1d05ee2ddba
Song S.H. <37033795+clastro@users.noreply.github.com>
2024-11-10 23:47:17 +0900

    Create Conv1D.py

---
+++ tensorflow/CNN/Conv1D.py
@@ -0,0 +1,24 @@
+"""
+tf.keras.layers.Conv1D(
+    filters,
+    kernel_size,
+    strides=1,
+    padding='valid',
+    data_format=None,
+    dilation_rate=1,
+    groups=1,
+    activation=None,
+    use_bias=True,
+    kernel_initializer='glorot_uniform',
+    bias_initializer='zeros',
+    kernel_regularizer=None,
+    bias_regularizer=None,
+    activity_regularizer=None,
+    kernel_constraint=None,
+    bias_constraint=None,
+    **kwargs
+)
+"""
+
+tf.keras.layers.Conv1D(self.sig_dims, kernel_size=self.filt, strides = self.strides,padding='same')
+
