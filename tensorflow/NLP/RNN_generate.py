1660e0a2ad596839457c649c045efa4a632a1425
Song S.H. <37033795+clastro@users.noreply.github.com>
2025-01-08 23:51:38 +0900

    Create RNN_generate.py

---
+++ tensorflow/NLP/RNN_generate.py
@@ -0,0 +1,25 @@
+example_texts = ['abcdefg', 'xyz']
+
+chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
+# <tf.RaggedTensor [[b'a', b'b', b'c', b'd', b'e', b'f', b'g'], [b'x', b'y', b'z']]>
+
+ids_from_chars = tf.keras.layers.StringLookup(
+    vocabulary=list(vocab), mask_token=None)
+ids = ids_from_chars(chars)
+ids
+#<tf.RaggedTensor [[40, 41, 42, 43, 44, 45, 46], [63, 64, 65]]>
+
+tf.strings.reduce_join(chars, axis=-1).numpy()
+
+#array([b'abcdefg', b'xyz'], dtype=object)
+
+def text_from_ids(ids):
+  return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
+
+all_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
+all_ids
+
+#<tf.Tensor: shape=(1115394,), dtype=int64, numpy=array([19, 48, 57, ..., 46,  9,  1])>
+
+ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
+
