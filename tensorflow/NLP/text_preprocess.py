c977e9948f28e24045748a3d7e4160bf7d6204f4
Song S.H. <37033795+clastro@users.noreply.github.com>
2025-03-05 23:56:16 +0900

    Create text_preprocess.py

---
+++ tensorflow/NLP/text_preprocess.py
@@ -0,0 +1,24 @@
+# Create a custom standardization function to strip HTML break tags '<br />'.
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+  return tf.strings.regex_replace(stripped_html,
+                                  '[%s]' % re.escape(string.punctuation), '')
+
+
+# Vocabulary size and number of words in a sequence.
+vocab_size = 10000
+sequence_length = 100
+
+# Use the text vectorization layer to normalize, split, and map strings to
+# integers. Note that the layer uses the custom standardization defined above.
+# Set maximum_sequence length as all samples are not of the same length.
+vectorize_layer = TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=vocab_size,
+    output_mode='int',
+    output_sequence_length=sequence_length)
+
+# Make a text-only dataset (no labels) and call adapt to build the vocabulary.
+text_ds = train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(text_ds)
